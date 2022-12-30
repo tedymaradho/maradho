0083072356540060e3524f37753f59693f166b8b
Kodinge <maradho@gmail.com>
2022-12-31 06:17:49 +0700

    add dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Logo from "../assets/logo.png";
 import { ButtonInverseSmall } from "../styles/Button";
 import { device } from "../styles/Device";
 import { FcMenu } from "react-icons/fc";
+import { AiOutlineClose } from "react-icons/ai";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -39,14 +40,29 @@ const LogoName = styled.span`
 `;
 
 const Navlink = styled.div`
+  display: flex;
+
   @media ${device.mobileS} {
-    display: none;
+    flex-direction: column;
+    margin-top: 200px;
   }
   @media ${device.tablet} {
-    display: flex;
+    flex-direction: row;
     align-items: center;
-    gap: 10px;
     opacity: 80%;
+    margin-top: 0;
+  }
+
+  gap: 10px;
+`;
+
+const ButtonLink = styled(ButtonInverseSmall)`
+  @media ${device.mobileS} {
+    background-color: transparent;
+    z-index: 3;
+  }
+  @media ${device.tablet} {
+    background-color: ${(props) => props.theme.bg};
   }
 `;
 
@@ -67,6 +83,42 @@ const MenuIcon = styled(FcMenu)`
   }
 `;
 
+const CloseIcon = styled(AiOutlineClose)`
+  padding: 7px;
+  background-color: white;
+  border-radius: 50%;
+  z-index: 3;
+
+  @media ${device.mobileS} {
+    display: inline-block;
+  }
+  @media ${device.tablet} {
+    display: none;
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme.bg};
+  }
+`;
+
+const BackdropMenu = styled.div`
+  width: 100vw;
+  height: 350px;
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  right: 0;
+  background-color: white;
+  opacity: 0.95;
+
+  @media ${device.mobileS} {
+    display: inline-block;
+  }
+  @media ${device.tablet} {
+    display: none;
+  }
+`;
+
 function Navbar() {
   return (
     <Container>
@@ -75,14 +127,16 @@ function Navbar() {
         <LogoName>Maradho</LogoName>
       </LogoBox>
       <Navlink>
-        <ButtonInverseSmall>Home</ButtonInverseSmall>
-        <ButtonInverseSmall>Services</ButtonInverseSmall>
-        <ButtonInverseSmall>Portfolio</ButtonInverseSmall>
-        <ButtonInverseSmall>Testimonials</ButtonInverseSmall>
-        <ButtonInverseSmall>Tools</ButtonInverseSmall>
-        <ButtonInverseSmall>Contact</ButtonInverseSmall>
+        <ButtonLink>Home</ButtonLink>
+        <ButtonLink>Services</ButtonLink>
+        <ButtonLink>Portfolio</ButtonLink>
+        <ButtonLink>Testimonials</ButtonLink>
+        <ButtonLink>Tools</ButtonLink>
+        <ButtonLink>Contact</ButtonLink>
       </Navlink>
       <MenuIcon size="2.5em" />
+      <CloseIcon size="2.5em" />
+      <BackdropMenu></BackdropMenu>
     </Container>
   );
 }
